42fd147946557684dfd87c0ad29c8ef93f3d5590
YASH JADHAV <yashjadhav654321@gmail.com>
2026-01-21 17:19:22 +0530

    i

--- Pattern5.java
+++ Pattern5.java
@@ -3,7 +3,6 @@ public class Pattern5{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of rows:");
-
         int n = sc.nextInt();
 
         for(int i=n;i>0;i--){
